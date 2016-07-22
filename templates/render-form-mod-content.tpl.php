43a356c3598853283f61c2486da07cd2c878edf2
ada_messer <ladydowaakhin@gmail.com>
2016-07-22 15:03:33 +0300

    fixed some bugs

--- templates/render-form-mod-content.tpl.php
+++ templates/render-form-mod-content.tpl.php
@@ -1,9 +1,9 @@
 <div <?php print $attributes; ?> class="<?php print $classes;?>">
   <div class="textfield-class-wrapper">
-    <?php print render($form['render_form_mod_brand']); ?>
+    <?php //print render($form['render_form_mod_brand']); ?>
   </div>
   <div class="select-class-wrapper">
-    <?php print render($form['render_form_mod_bargain']); ?>
+    <?php //print render($form['render_form_mod_bargain']); ?>
   </div>
   <?php print drupal_render_children($form); ?>
 </div>
